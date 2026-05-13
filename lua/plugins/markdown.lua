5e9cf4e5ec97a6b9ca037714789a8b54134fd4cf
Edson Jaramillo <edsonajaramillo@gmail.com>
2026-05-12 19:38:16 -0500

    feat(markdown): add markview plugin for markdown preview
    - add markview.nvim to plugin list and provide basic setup
    - update nvim-pack-lock.json to include markview dependency

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,8 @@
+-- ######### INSTALLATION #########
+vim.pack.add({
+	"https://github.com/OXY2DEV/markview.nvim",
+})
+
+-- ######### CONFIG #########
+local markview = require("markview")
+markview.setup()
