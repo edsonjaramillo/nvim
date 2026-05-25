db66c3d7f265a66aa602ea935214a50463f23a9e
Edson Jaramillo <edsonajaramillo@gmail.com>
2026-05-24 21:55:04 -0500

    chore(plugins): remove markview markdown plugin
    - remove require from lua/plugins/init.lua
    - delete lua/plugins/markdown.lua and associated config

--- lua/plugins/markdown.lua
+++
@@ -1,8 +0,0 @@
--- ######### INSTALLATION #########
-vim.pack.add({
-	"https://github.com/OXY2DEV/markview.nvim",
-})
-
--- ######### CONFIG #########
-local markview = require("markview")
-markview.setup()
